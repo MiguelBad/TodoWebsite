3e8c69fcd6503791f2aa0cf7f1fd47b2c5148e7f
Miguel Badayos <badayosbiboy@gmail.com>
2024-06-08 22:35:36 +0930

    improving nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,34 @@
 import './index.css';
 import './Main.js';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 function TodoApp() {
     return (
         <nav className='side-navigation'>
-            <h1 className='website-title'>Todo App</h1>
-            <section className='profile-container'>
-                <p className='introduction'>Hello,</p>
-                <div className='profile-pic-name-container'>
-                    <div className='profile-picture'>
-                        <AccountBoxIcon />
+            <section className='top-navigation-part'>
+                <section className='menu-hamburger-container'>
+                    <h1 className='menu-title'>Menu</h1>
+                    <div className='hamburger-menu'>
+                        <div className='top-hamburger'></div>
+                        <div className='middle-hamburger'></div>
+                        <div className='bottom-hamburger'></div>
                     </div>
+                </section>
+                <section className='profile-container'>
+                    <p className='introduction'>Hello,</p>
                     <h2 className='username'>User Name</h2>
-                </div>
+                </section>
+                <hr />
+                <section className='todo-list-container'>
+                    <h2 className='todo-list-heading'>Todo List</h2>
+                    <ul className='todo-list'>
+                        <li className='todo'>Todo Title Sample</li>
+                    </ul>
+                    <p className='add-new-todo'>+ Add New</p>
+                </section>
             </section>
-            <section className='todo-list-container'>
-                <h2 className='todo-list-heading'>Todo List</h2>
-                <ul className='todo-list'>
-                    <li className='todo'>Todo Title Sample</li>
-                </ul>
+            <section className='bottom-navigation-part'>
+                <p>Light Mode</p>
+                <p>Settings</p>
             </section>
         </nav>
     );
