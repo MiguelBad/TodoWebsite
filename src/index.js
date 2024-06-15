cf4a606c6d31dfd9f33d2bb97e4c1d28030a1ac7
Miguel Badayos <badayosbiboy@gmail.com>
2024-06-15 20:03:39 +0930

    wrapped whole app inside the context hook

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './Main';
 import NavBar from './NavBar';
+import { CurrentCategory } from './TodoContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <NavBar />
-    <Main />
-  </React.StrictMode>
+    <React.StrictMode>
+        <CurrentCategory>
+            <NavBar />
+            <Main />
+        </CurrentCategory>
+    </React.StrictMode>
 );
